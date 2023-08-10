72044ff2dbd2204a9585051c32ccaa54301d2bb1
Svetlana Shlekht <sv.2502@mail.ru>
2023-08-10 08:36:03 +0300

    fit

--- src/index.js
+++ src/index.js
@@ -38,8 +38,87 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = [];
+  let arr = [];
+  let subArr = [];
+  let newResult = [];
+
+const MORSE_TABLE = {
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
+  ' ': " ",
+};
+
+// разбиваем строку на массив по 2 символа
+for (let i = 0; i < expr.length; i+=2) {
+  arr.push(expr[i] + expr[i + 1] );
+};
+
+
+// разбила на массив по 5 пар
+function chunkArray(arr, chunk) {
+for (let i = 0; i < arr.length; i += chunk) {
+  subArr.push(arr.slice(i, i + chunk));
 }
+}
+chunkArray(arr, 5);
+
+// меняю 0и1 на символы, а затем на буквы
+for (let k=0; k<subArr.length; k++) {
+   for (let j=0; j<5; j++)  {
+     if (subArr[k][j] == '00') {
+        subArr[k].splice(j, 1, '');
+     } else if (subArr[k][j] == '10') {
+       subArr[k].splice(j, 1, '.');
+     } else if (subArr[k][j] == '11') {
+       subArr[k].splice(j, 1, '-');
+     } else if (subArr[k][j] == '**') {
+        subArr[k].splice(j, 10, ' ');
+     }
+   } ;
+   result[k] = subArr[k].join("");
+   newResult += MORSE_TABLE[result[k]];
+}
+// console.log(newResult);
+return newResult;
+
+};
+    // write your solution here
+
 
 module.exports = {
     decode
